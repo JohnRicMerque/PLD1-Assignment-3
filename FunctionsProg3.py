c5de1dc23b345ab06a1da528133e8a6d9bdcbb2e
unknown <you@example.com>
2021-10-31 14:04:30 +0800

    Program complete: successfully uses functions

--- FunctionsProg3.py
+++ FunctionsProg3.py
@@ -9,5 +9,18 @@ def getAplPrice():
     aplPrice = float(input("Enter the price of one apple: "))
     return aplPrice
 
-getMoney()
-getAplPrice()
+def getMaxApples(money, price):
+    maxApls = int(money//price)
+    return maxApls
+
+def getChange(money, price):
+    change = money%price
+    return change
+
+# calling functions and storing them in variables
+amount_of_money = getMoney()
+apple_price = getAplPrice()
+max_apples = getMaxApples(amount_of_money, apple_price)
+total_change = getChange(amount_of_money, apple_price)
+
+print(f"You can buy {max_apples} apples and your change is {total_change} pesos.")
