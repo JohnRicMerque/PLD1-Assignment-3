43f63e3ca91aeb5eeae4d34da84e6ee08b51849f
unknown <you@example.com>
2021-10-31 12:58:45 +0800

    Functions are working

---
+++ FunctionsProg2.py
@@ -0,0 +1,13 @@
+# Program2 using functions
+
+# defining functions
+def getApls():
+    nOfApls = int(input("Enter the number of apples you want to buy: "))
+    return nOfApls
+
+def getOrgs():
+    nOfOrgs = int(input("Enter the number of oranges you want to buy: "))
+    return nOfOrgs
+
+getApls()
+getOrgs()
