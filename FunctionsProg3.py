d92f3e841c4c9aef2da0a093b7b686bae2f9f79d
unknown <you@example.com>
2021-10-31 13:37:55 +0800

    functions are working

---
+++ FunctionsProg3.py
@@ -0,0 +1,13 @@
+# Program3 using functions
+
+# defining functions
+def getMoney():
+    moneyAmount = float(input("How much money do you have?: "))
+    return moneyAmount
+
+def getAplPrice():
+    aplPrice = float(input("Enter the price of one apple: "))
+    return aplPrice
+
+getMoney()
+getAplPrice()
