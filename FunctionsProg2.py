01ec91a2dcb3683e25b0b985136221d77aa37faa
unknown <you@example.com>
2021-10-31 13:31:33 +0800

    Program Complete: successfully used functions

--- FunctionsProg2.py
+++ FunctionsProg2.py
@@ -9,5 +9,15 @@ def getOrgs():
     nOfOrgs = int(input("Enter the number of oranges you want to buy: "))
     return nOfOrgs
 
-getApls()
-getOrgs()
+def getPrice(apl_number, org_number):
+    apl_price = apl_number * 20
+    org_price = org_number * 25
+    total = apl_price + org_price
+    return total
+
+# calling functions and storing in variables
+applesN = getApls()
+orangesN = getOrgs()
+totalPrice = getPrice(applesN, orangesN)
+
+print(f"The total amount is {totalPrice}.")
